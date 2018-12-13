377e774652bce14cce855b2d0d74c0f1697295d7
Thiago Paim <thiagopain@gmail.com>
2018-12-13 12:06:35 -0200

    update gulp 4 e webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,108 @@
 // gulp dependecies
-const gulp            = require('gulp');
-const plumber         = require('gulp-plumber');
-const pug             = require('gulp-pug');
-const sass            = require('gulp-sass');
-const imagemin        = require('gulp-imagemin');
-const uglify          = require('gulp-uglify');
-const sourcemaps      = require('gulp-sourcemaps');
-const autoprefixer    = require('gulp-autoprefixer');
-const concat          = require('gulp-concat');
-const browserify      = require('browserify');
-const babelify        = require('babelify');
-const source          = require('vinyl-source-stream');
-const buffer          = require('vinyl-buffer');
-const version         = require('gulp-version-number');
-const browserSync     = require('browser-sync').create();
+const gulp = require('gulp')
+const pug = require('gulp-pug')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
+const imagemin = require('gulp-imagemin')
+const webpack = require('webpack-stream')
+const del = require('del')
+const browserSync = require('browser-sync')
+const server = browserSync.create()
 
 // paths
-const srcImages       = 'src/images/**/*';
-const srcStyles       = 'src/styles/main.scss';
-const srcScripts      = 'src/scripts/main.js';
-const jsFiles         = [srcScripts];
-
-// paths watch
-const stylesWatch     = 'src/styles/**/*.scss';
-const scriptsWatch    = 'src/scripts/**/*.js';
-const viewsWatch      = 'src/**/*.pug';
-
-// version
-const versionConfig = {
-  'value': '%TS%',
-  'append': {
-    'key': 'v',
-    'to': ['css', 'js']
+let paths = {
+  views: {
+    watch: 'src/**/*.pug',
+    dest: 'build/'
+  },
+  styles: {
+    src: 'src/styles/main.scss',
+    watch: 'src/styles/**/*.scss',
+    dest: 'build/'
+  },
+  scripts: {
+    src: 'src/scripts/main.js',
+    watch: 'src/scripts/**/*.js',
+    dest: 'build/'
+  },
+  images: {
+    src: 'src/images/**/*.{jpg,jpeg,png,svg}',
+    watch: 'src/images/**/*',
+    dest: 'build/images/'
   }
 }
 
-// pug, html, php... views
-gulp.task('views', () => {
-  gulp.src(viewsWatch)
-      .pipe(plumber())
-      .pipe(pug({ pretty: false }))
-      .pipe(version(versionConfig))
-      .pipe(gulp.dest('./build/'))
-      .pipe(browserSync.stream())
-});
+function clean () {
+  return del(['build'])
+}
 
-// compress images
-gulp.task('images', () => {
-  gulp.src(srcImages)
+function views () {
+  return gulp.src(paths.views.watch)
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(paths.views.dest))
+}
+
+function styles () {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(concat('build.css'))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+function scripts () {
+  return gulp.src(paths.scripts.src)
+    .pipe(webpack({
+      output: {
+        filename: 'build.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
+      devtool: 'source-map',
+      mode: 'production'
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+function images () {
+  return gulp.src(paths.images.src)
     .pipe(imagemin())
-    .pipe(gulp.dest('./build/images/'))
-    .pipe(browserSync.stream())
-});
+    .pipe(gulp.dest(paths.images.dest))
+}
 
-// styles
-gulp.task('styles', () => {
-  gulp.src(srcStyles)
-      .pipe(plumber())
-      .pipe(sourcemaps.init())
-      .pipe(sass({ 
-        errorLogToConsole: true,
-        outputStyle: 'compressed'
-      }))
-      .on('error', console.error.bind( console ))
-      .pipe(autoprefixer({ 
-        browsers: ['last 2 versions'],
-        cascade: false
-      }))
-      .pipe(concat('build.css'))
-      .pipe(sourcemaps.write('./'))
-      .pipe(version(versionConfig))
-      .pipe(gulp.dest('./build/'))
-      .pipe(browserSync.stream())
-});
+function reload (done) {
+  server.reload()
+  done()
+}
 
-// scripts
-gulp.task('scripts', () => {
-  jsFiles.map((entry) => {
-      return browserify({
-        entries: [entry]
-      })
-      .transform(babelify, {
-        presets: ['env']
-      })
-      .bundle()
-      .pipe(source(entry))
-      .pipe(buffer())
-      .pipe(plumber())
-      .pipe(sourcemaps.init({ 
-        loadMaps: true 
-      }))
-      .pipe(uglify())
-      .pipe(concat('build.js'))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./build/'))
-      .pipe(browserSync.stream())
-  })
-});
-
-// browser sync
-gulp.task('browser-sync', () => {
-  browserSync.init({
-    open: false,
-    injectChanges: true,
-    // proxy: 'http://something.localhost'
-    // or:
+function serve (done) {
+  server.init({
     server: {
-      baseDir: './build/'
+      baseDir: 'build/'
     }
   })
-});
+  done()
+}
 
-// get shit done
-// init with gulp run
-gulp.task('default', ['styles', 'scripts', 'views', 'images']);
-gulp.task('run', ['default', 'browser-sync'], () => {
-  gulp.watch(stylesWatch, ['styles', browserSync.reload]);
-  gulp.watch(scriptsWatch, ['scripts', browserSync.reload]);
-  gulp.watch(viewsWatch, ['views', browserSync.reload]);
-  gulp.watch(srcImages, ['imges', browserSync.reload]);
-});
+function watch () {
+  gulp.watch(paths.views.watch, gulp.series(views, reload))
+  gulp.watch(paths.scripts.watch, gulp.series(scripts, reload))
+  gulp.watch(paths.styles.watch, gulp.series(styles, reload))
+  gulp.watch(paths.images.watch, gulp.series(images, reload))
+}
+
+let build = gulp.series(clean, gulp.parallel(views, styles, scripts, images, serve, watch))
+
+gulp.task('build', build)
+gulp.task('default', build)
